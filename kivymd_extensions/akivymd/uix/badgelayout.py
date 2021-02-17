2e2f313257fbc6b42fcf79530f191d4be88ffe20
Sina Namadian <quitegreensky@gmail.com>
2021-02-16 17:29:36 -0800

    PEP8 `badgelayout`

--- kivymd_extensions/akivymd/uix/badgelayout.py
+++ kivymd_extensions/akivymd/uix/badgelayout.py
@@ -14,54 +14,53 @@ from kivymd.theming import ThemableBehavior
 Builder.load_string(
     """
 <BadgeItem>
-    size_hint: None,None
+    size_hint: None, None
     padding: dp(5)
     size: self.size if root.text else [dp(20), dp(20)]
-    opacity: 1 if self.badge_disabled==False else 0
+    opacity: 1 if self.badge_disabled == False else 0
 
     pos:
-        (self.parent.x  -self.width*(root.offset), self.parent.y+ self.parent.height  -self.height*(1-root.offset)) if root.position=='left'\
-        else (self.parent.x +self.parent.width -self.width*(1-root.offset) , self.parent.y +self.parent.height -self.height*(1-root.offset))
+        (self.parent.x - self.width * (root.offset), self.parent.y + self.parent.height - self.height * (1 - root.offset)) if root.position == 'left' \
+        else (self.parent.x + self.parent.width - self.width * (1 - root.offset), self.parent.y + self.parent.height - self.height * (1 - root.offset))
 
     canvas.before:
         Color:
-            rgba:  root.bg_color if root.bg_color else root.theme_cls.bg_normal
+            rgba: root.bg_color if root.bg_color else root.theme_cls.bg_normal
         RoundedRectangle:
-            pos:self.pos
+            pos: self.pos
             size: self.size
-            radius: [root.height/2, root.height/2, root.height/2, root.height/2]
+            radius: [root.height / 2, ]
 
         Color:
             rgba: root.badgeitem_color if root.badgeitem_color else root.theme_cls.accent_color
         RoundedRectangle:
             pos:
-                [self.pos[0]+ root.badgeitem_padding/2  , self.pos[1]+ root.badgeitem_padding/2]
-            size: [ self.size[0]-root.badgeitem_padding, self.size[1]-root.badgeitem_padding ]
-            radius: [root.height/2, root.height/2, root.height/2, root.height/2]
+                [self.pos[0] + root.badgeitem_padding / 2, self.pos[1] + root.badgeitem_padding/2]
+            size: [self.size[0] - root.badgeitem_padding, self.size[1] - root.badgeitem_padding]
+            radius: [root.height / 2, ]
     Label:
-        size_hint: None,None
-        size: self.texture_size[0]+dp(10), self.texture_size[1]
+        size_hint: None, None
+        size: self.texture_size[0] + dp(10), self.texture_size[1]
         halign: 'center'
         valign: 'center'
         font_size: dp(13)
         bold: root.bold
         pos_hint: {'center_x': .5, 'center_y': .5}
         text: root.text
-        color: 1,1,1,1
+        color: 1, 1, 1, 1
 
 <AKBadgeLayout>:
-
-    size_hint: None,None
+    size_hint: None, None
     size: box.size
     BadgeContent:
         id: box
         pos: root.pos
-        size_hint: None,None
+        size_hint: None, None
         size: self.minimum_size
 
     BadgeItem:
         id: badge
-        size_hint: None,None
+        size_hint: None, None
         size: self.minimum_size
         bg_color: root.bg_color
         badgeitem_padding: root.badgeitem_padding
@@ -71,7 +70,6 @@ Builder.load_string(
         bold: root.bold
         offset: root.offset
         badge_disabled: root.badge_disabled
-
     """
 )
 
