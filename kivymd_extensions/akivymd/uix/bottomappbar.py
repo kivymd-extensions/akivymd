a2893735bb9c452181647779ee1cced08eff5012
Sina Namadian <quitegreensky@gmail.com>
2021-02-16 17:40:06 -0800

    PEP8 bottomappbar

--- kivymd_extensions/akivymd/uix/bottomappbar.py
+++ kivymd_extensions/akivymd/uix/bottomappbar.py
@@ -9,29 +9,29 @@ from kivymd.uix.behaviors import MagicBehavior
 Builder.load_string(
     """
 <AKFloatingRoundedAppbarItemBase>:
-    orientation: 'vertical'
+    orientation: "vertical"
     size_hint: None, None
     height: self.parent.height - dp(5)
     width: self.minimum_width
-    pos_hint: {'center_x': .5, 'center_y': .5}
+    pos_hint: {"center_x": .5, "center_y": .5}
 
 
 <AKFloatingRoundedAppbarButtonItem>:
     MDIcon:
         icon: root.icon
-        halign: 'center'
-        valign: 'center'
-        theme_text_color: 'Custom'
+        halign: "center"
+        valign: "center"
+        theme_text_color: "Custom"
         text_color: root.icon_color if root.icon_color else 1, 1, 1, 1
         font_size: dp(20)
-        pos_hint: {'center_x': .5, 'center_y': .5}
+        pos_hint: {"center_x": .5, "center_y": .5}
         size_hint: None, None
         size: self.font_size, self.font_size
 
     Label:
         text: root.text
-        halign: 'center'
-        valign: 'center'
+        halign: "center"
+        valign: "center"
         font_size: dp(10)
         color: root.text_color if root.text_color else 1, 1, 1, 1
         size_hint: None,None
@@ -44,7 +44,7 @@ Builder.load_string(
     BoxLayout:
         size_hint: None, None
         size : [self.parent.height - dp(2), self.parent.height - dp(2)] if not root.text else [dp(20), dp(20)]
-        pos_hint: {'center_x': .5, 'center_y': .5}
+        pos_hint: {"center_x": .5, "center_y": .5}
         canvas.after:
             Color:
                 rgba: 1, 1, 1, 1
@@ -55,17 +55,17 @@ Builder.load_string(
 
     Label:
         text: root.text
-        halign: 'center'
-        valign: 'center'
+        halign: "center"
+        valign: "center"
         font_size: dp(10)
         color: root.text_color if root.text_color else 1, 1, 1, 1
-        size_hint: None,None
+        size_hint: None, None
         size: self.texture_size
 
 <AKFloatingRoundedAppbar>:
     size_hint: None,None
     size: self.minimum_width, dp(40)
-    pos_hint: {'center_x': .5}
+    pos_hint: {"center_x": .5}
     y: dp(10)
     spacing: dp(40)
     padding: dp(40)
