d2a508d4bd845778f59029ec24eeb34c9461a23b
Guhan Sensam <guhansensam@gmail.com>
2021-04-27 20:35:44 +0530

    Changes

--- examples/full_example/screens/cardstack.py
+++ examples/full_example/screens/cardstack.py
@@ -1,11 +1,11 @@
 from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
-from kivymd_extensions.akivymd.uix.cardstack import AKCardStack
 
 Builder.load_string(
 """
 <CardStack>:
     AKCardStack:
+        id:cardstack
         pos_hint:{'center_x':.5, 'center_y':.5}
         size: 800,800
         transition: 'in_out_circ'
@@ -16,9 +16,5 @@ Builder.load_string(
 """)
 
 class CardStack(MDScreen):
-    def __init__(self, *kwargs):
-        super().__init__(*kwargs)
-        self.cardstack = AKCardStack()
-
-        def change(self):
-            self.cardstack.change()
+    def change(self):
+        self.ids.cardstack.change()
