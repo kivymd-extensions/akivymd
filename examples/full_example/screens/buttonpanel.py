084dee258ccaa0288eb8919ba74dd9f660302dd2
Sina Namadian <quitegreensky@gmail.com>
2021-05-03 13:32:20 +0430

    Button panel reformatted

--- examples/full_example/screens/buttonpanel.py
+++ examples/full_example/screens/buttonpanel.py
@@ -4,32 +4,33 @@ from kivymd.uix.screen import MDScreen
 Builder.load_string(
     """
 <ButtonPanel>:
+
     MyToolbar:
         id: _toolbar
         pos_hint: {"top": 1}
 
     AKButtonPanel:
-        pos:250,200
-        icon:'plus'
-        selectable:True
+        pos: 250, 200
+        icon:"plus"
+        selectable: True
 
         MDIconButton:
-            icon:'minus'
+            icon: "minus"
             theme_text_color: "Custom"
-            text_color:1,1,0,1
+            text_color: 1, 1, 0, 1
 
         MDIconButton:
-            icon:'calendar'
+            icon: "calendar"
             theme_text_color: "Custom"
-            text_color:1,0,0,1
+            text_color: 1, 0, 0, 1
 
         MDIconButton:
-            icon:'android'
+            icon: "android"
             theme_text_color: "Custom"
-            text_color:0,1,1,1
+            text_color: 0, 1, 1, 1
     """
-
 )
+
 
 class ButtonPanel(MDScreen):
     pass
--- kivymd_extensions/akivymd/uix/buttonpanel.py
+++ kivymd_extensions/akivymd/uix/buttonpanel.py
@@ -1,17 +1,16 @@
-from kivy.lang import Builder
-from kivy.uix.relativelayout import RelativeLayout
-from kivymd.uix.button import MDIconButton
-from kivymd.uix.button import MDFloatingActionButton
-from kivymd.theming import ThemableBehavior
 from kivy.animation import Animation
+from kivy.lang import Builder
 from kivy.metrics import dp
 from kivy.properties import (
+    BooleanProperty,
     ColorProperty,
     ListProperty,
     NumericProperty,
     StringProperty,
-    BooleanProperty,
 )
+from kivy.uix.relativelayout import RelativeLayout
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.button import MDFloatingActionButton, MDIconButton
 
 Builder.load_string(
     """
@@ -19,28 +18,29 @@ Builder.load_string(
     pos:self.pos
     size_hint:None,None
     height:dp(56)
+
     canvas:
         Color:
-            rgba:self.theme_cls.primary_color if not self.rectangle_color else self.rectangle_color
+            rgba: self.theme_cls.primary_color if not self.rectangle_color else self.rectangle_color
         RoundedRectangle:
-            pos:self.pos[0],self.pos[1]-self.height + dp(56)
+            pos: self.pos[0], self.pos[1] - self.height + dp(56)
             size:dp(56),self.height
             radius:self.radius
 
     MDFloatingActionButton:
-        id:main_button
-        icon:root.icon
-        pos:root.pos
-        on_release:root._show()
-        md_bg_color:root.theme_cls.primary_dark if not root.main_button_color else root.main_button_color
-        text_color:root.theme_cls.text_color if not root.icon_color else root.icon_color
+        id: main_button
+        icon: root.icon
+        pos: root.pos
+        on_release: root._show()
+        md_bg_color: root.theme_cls.primary_dark if not root.main_button_color else root.main_button_color
+        text_color: root.theme_cls.text_color if not root.icon_color else root.icon_color
 
 
     RelativeLayout:
-        id:icon_holder
-        pos:root.pos[0], root.pos[1]
-        width:dp(56)
-        size_hint:None,None
+        id: icon_holder
+        pos: root.pos[0], root.pos[1]
+        width: dp(56)
+        size_hint: None, None
 
 """
 )
@@ -94,16 +94,16 @@ class AKButtonPanel(ThemableBehavior, RelativeLayout):
         else:
             if isinstance(widget, MDIconButton):
                 widget.bind(on_press=self._button_mover)
-                widget.pos_hint = {'center_x':.5}
+                widget.pos_hint = {"center_x": 0.5}
                 self.ids.icon_holder.add_widget(widget)
 
     def _show(self, *args):
         if self.open:
             if not self._added:
                 for icon in self.ids.icon_holder.children:
-                    Animation(pos=(0, 0), d=self.anim_duration, t=self.animation).start(
-                        icon
-                    )
+                    Animation(
+                        pos=(0, 0), d=self.anim_duration, t=self.animation
+                    ).start(icon)
                 else:
                     Animation(
                         height=dp(56), d=self.anim_duration, t=self.animation
@@ -118,16 +118,18 @@ class AKButtonPanel(ThemableBehavior, RelativeLayout):
                 ).start(icon)
                 self.z -= dp(56)
             else:
-                Animation(height=-self.z, d=self.anim_duration, t=self.animation).start(
-                    self
-                )
+                Animation(
+                    height=-self.z, d=self.anim_duration, t=self.animation
+                ).start(self)
                 self.open = True
 
     def _button_mover(self, widget, *args):
         if self.selectable:
             if not self._added:
                 self.top_icon = MDIconButton(
-                    icon=self.icon, theme_text_color="Custom", pos_hint={'center_x':.5}
+                    icon=self.icon,
+                    theme_text_color="Custom",
+                    pos_hint={"center_x": 0.5},
                 )
                 self.top_icon.text_color = (
                     self.theme_cls.text_color
