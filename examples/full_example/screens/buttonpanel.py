b47637d60100e396a83023e8377db450e02a4b6a
Guhan Sensam <guhansensam@gmail.com>
2021-05-03 09:27:39 +0530

    Fixed Example

--- examples/full_example/screens/buttonpanel.py
+++ examples/full_example/screens/buttonpanel.py
@@ -3,25 +3,30 @@ from kivymd.uix.screen import MDScreen
 
 Builder.load_string(
     """
-<ButtonPanel@AKButtonPanel>:
-    pos:60,150
-    icon:'plus'
-    selectable:True
+<ButtonPanel>:
+    MyToolbar:
+        id: _toolbar
+        pos_hint: {"top": 1}
 
-    MDIconButton:
-        icon:'minus'
-        theme_text_color: "Custom"
-        text_color:1,1,0,1
+    AKButtonPanel:
+        pos:250,200
+        icon:'plus'
+        selectable:True
 
-    MDIconButton:
-        icon:'calendar'
-        theme_text_color: "Custom"
-        text_color:1,0,0,1
+        MDIconButton:
+            icon:'minus'
+            theme_text_color: "Custom"
+            text_color:1,1,0,1
 
-    MDIconButton:
-        icon:'android'
-        theme_text_color: "Custom"
-        text_color:0,1,1,1
+        MDIconButton:
+            icon:'calendar'
+            theme_text_color: "Custom"
+            text_color:1,0,0,1
+
+        MDIconButton:
+            icon:'android'
+            theme_text_color: "Custom"
+            text_color:0,1,1,1
     """
 
 )
